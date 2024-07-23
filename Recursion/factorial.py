e7a85c7654fef0f37a6b6b7acd52a8389b072d3a
PRAVEENKUMARRAJENDRAN <praveenrajendran2109@gmail.com>
2024-07-23 19:21:13 -0400

    Factorial using recursion

--- Recursion/factorial.py
+++ Recursion/factorial.py
@@ -1,9 +1,9 @@
-
-
-
 def factorial(n):
     if n == 1:
-        return 1
-    return n * factorial(n-1)
+        return 1 
+    
+    n = n * factorial(n-1)
+    return n
 
-print(factorial(3))
+
+print(factorial(5))
